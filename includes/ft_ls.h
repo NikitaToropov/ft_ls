ea85cbff5378b4a221529b8fbc70f72256ca75b8
NikitaToropov <nikkyniknikita@gmail.com>
2021-01-10 14:05:36 +0300

    norm is ok

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: cmissy <cmissy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/20 14:13:50 by cmissy            #+#    #+#             */
-/*   Updated: 2021/01/10 13:46:07 by cmissy           ###   ########.fr       */
+/*   Updated: 2021/01/10 14:04:02 by cmissy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,23 +28,23 @@
 
 # define MEM_CHECK(x) if (!x) exit(2)
 
-/**
- * Flags error status.
- */
+/*
+** Flags error status.
+*/
 # define ILLEGAL_OPTION			1
 
-/**
- * Nodes statuses.
- */
+/*
+** Nodes statuses.
+*/
 # define NO_SUCH_FILE_OR_DIR	2
 # define PERMISSION_DENIED		4
 # define DIRECTORY				8
 # define DUMMY_DIR				16
 # define FILE					32
 
-/**
- * Utility definitions.
- */
+/*
+** Utility definitions.
+*/
 # define TRUE					1
 # define FALSE					0
 # define MORE					1
@@ -53,154 +53,143 @@
 # define SYM_LINK_ARROW			" -> "
 # define DEFAULT_BUFF_SIZE		16
 
-/**
- * Printing mods.
- */
+/*
+** Printing mods.
+*/
 # define WO_DIR_DESCRIPTION		0
 # define WO_LINE_BREAK			1
 # define W_LINE_BREAK			2
 
-/**
- * Formatting strings for dirs.
- */
-# define LB_NAME_LB_TOTAL_LB	"\n%s:\ntotal %li\n"
-# define NAME_LB_TOTAL_LB		"%s:\ntotal %li\n"
-# define LB_NAME_LB				"\n%s:\n"
-# define NAME_LB				"%s:\n"
-# define TOTAL_LB				"total %li\n"
-
-/**
- * Formatting strings for content.
- */
+/*
+** Formatting strings for content.
+*/
 # define L_FORMATTING			"%s %*u %-*s  %-*s  %*lli %s %s%s\n"
 # define G_FORMATTING			"%s %*u %6$-*s  %*lli %s %s%s\n"
 # define DEFAULT_FORMATTING		"%11$s\n"
 
-/**
- * All flags.
- */
-# define FLAGS					"lRartuUfg1" // todo del "1" flag
+/*
+** All flags. TODO dont forget about '1' flag
+*/
+# define FLAGS					"lRartuUfg1"
 
-typedef struct		s_dir_format
+typedef struct					s_dir_format
 {
-	size_t			num_of_files;
-	size_t			name_len;
-	size_t			owner_len;
-	size_t			size_len;
-	size_t			group_len;
-	size_t			num_of_links_len;
-}					t_dir_format;
+	size_t						num_of_files;
+	size_t						name_len;
+	size_t						owner_len;
+	size_t						size_len;
+	size_t						group_len;
+	size_t						num_of_links_len;
+}								t_dir_format;
 
-typedef struct		s_node
+typedef struct					s_node
 {
-	char			status;
+	char						status;
+	char						*name;
+	char						*path;
+	long int					total_size;
+	time_t						node_sec_time;
+	time_t						node_nsec_time;
+	char						date[13];
+	char						*sym_link;
+	nlink_t						num_of_links;
+	off_t						size_in_bytes;
+	char						*owner_name;
+	char						*group_name;
+	char						file_mod[20];
+	blkcnt_t					blocks;
+	struct s_dir_format			format;
+	struct s_node				*content;
+	struct s_node				*parent;
+	struct s_node				*next;
+}								t_node;
 
-	char			*name;
-	char			*path;
-	long int		total_size;
-
-	time_t			node_sec_time;
-	time_t			node_nsec_time;
-	char			date[13];
-
-	char			*sym_link;
-	nlink_t			num_of_links;
-	off_t			size_in_bytes;
-	char			*owner_name;
-	char			*group_name;
-	char			file_mod[20];
-
-	blkcnt_t		blocks;
-
-	struct s_dir_format	format;
-
-	struct s_node	*content;
-	struct s_node	*parent;
-	struct s_node	*next;
-}					t_node;
-
-typedef struct		s_quick_sort
+typedef struct					s_quick_sort
 {
-	t_node			*pivot;
-	t_node			*prev;
-	t_node			*cur;
-	t_node			*tail;
-	t_node			*tmp;
-}					t_quick_sort;
+	t_node						*pivot;
+	t_node						*prev;
+	t_node						*cur;
+	t_node						*tail;
+	t_node						*tmp;
+}								t_quick_sort;
 
-typedef struct		s_facade
+typedef struct					s_facade
 {
-	struct s_node	*dirs;
-	struct s_node	files_parent;
-	struct s_node	*invalid_nodes;
-}					t_facade;
+	struct s_node				*dirs;
+	struct s_node				files_parent;
+	struct s_node				*invalid_nodes;
+}								t_facade;
 
-/**
- * Utils.
- */
-unsigned short		get_flag_code(char flag);
-void				error_handler(char error_code, char *arg);
-char				*ft_pathjoin(char *first, char *second);
+/*
+** Utils.
+*/
+unsigned short					get_flag_code(char flag);
+void							error_handler(char error_code, char *arg);
+char							*ft_pathjoin(char *first, char *second);
 
-/**
- * "t_nodes" utils.
- */
-t_node				*new_t_dir(char *name, t_node *parent);
-void				insert_order_by(t_node **head, t_node *new,
-						unsigned short flags);
-void				del_line_of_nodes(t_node **head);
-char				is_dummy_dir(t_node *node);
+/*
+** "t_nodes" utils.
+*/
+t_node							*new_t_dir(char *name, t_node *parent);
+void							insert_order_by(t_node **head, t_node *new,
+									unsigned short flags);
+void							del_line_of_nodes(t_node **head);
+char							is_dummy_dir(t_node *node);
 
+/*
+** Stat handling functions.
+*/
+void							fill_time(t_node *node, unsigned short flags,
+									struct stat stt);
+void							fill_sym_link(t_node *node, struct stat stt);
+void							fill_file_mod(t_node *node, struct stat stt);
+void							fill_format(t_node *parent, t_node *curr);
 
-/**
- * Stat handling functions.
- */
-void				fill_time(t_node *node, unsigned short flags,
-						struct stat stt);
-void				fill_sym_link(t_node *node, struct stat stt);
-void				fill_file_mod(t_node *node, struct stat stt);
-void				fill_format(t_node *parent, t_node *curr);
+/*
+** t_node comparators.
+*/
+char							compare_by_date(const t_node *first,
+									const t_node *second);
+char							compare_by_date_reverse(const t_node *first,
+									const t_node *second);
+char							compare_lexicographic(const t_node *first,
+									const t_node *second);
+char							compare_lexicographic_reverse(
+									const t_node *first, const t_node *second);
+char							default_for_pushing_back(const t_node *first,
+									const t_node *second);
 
-/**
- * t_node comparators.
- */
-char				compare_by_date(const t_node *first, const t_node *second);
-char				compare_by_date_reverse(const t_node *first,
-						const t_node *second);
-char				compare_lexicographic(const t_node *first,
-						const t_node *second);
-char				compare_lexicographic_reverse(const t_node *first,
-						const t_node *second);
-char				default_for_pushing_back(const t_node *first,
-						const t_node *second);
-
-/**
- * Parsers.
- */
-unsigned short		flags_parser_facade(char ***argv);
-void				dir_parser_facade(char **argv, unsigned short flags);
-void					parse_the_dir(t_node *parent, unsigned short flags, char *printing_mod);
-void			simple_dir_printing(struct s_node *dirs_head,
+/*
+** Parsers.
+*/
+unsigned short					flags_parser_facade(char ***argv);
+void							dir_parser_facade(char **argv,
+									unsigned short flags);
+void							parse_the_dir(t_node *parent,
 									unsigned short flags, char *printing_mod);
-void			recursive_dir_printing(struct s_node *dirs_head,
+void							simple_dir_printing(struct s_node *dirs_head,
+									unsigned short flags, char *printing_mod);
+void							recursive_dir_printing(struct s_node *dirs_head,
 									unsigned short flags, char *printing_mod);
 
-/**
- * Sorting.
- */
-void				nodes_sorting_by_flags(t_node **head,
-										   unsigned short flags);
-t_node				*get_tail(t_node *node);
-t_node				*quick_sort_nodes_recur(t_node *head, t_node *end,
-						char comparator(const t_node *, const t_node *));
+/*
+** Sorting.
+*/
+void							nodes_sorting_by_flags(t_node **head,
+									unsigned short flags);
+t_node							*get_tail(t_node *node);
+t_node							*quick_sort_nodes_recur(t_node *head,
+									t_node *end, char comparator(const t_node *,
+									const t_node *));
 
-/**
- * Printing.
- */
-void	print_one_column(t_node *head, unsigned short flags);
-void	print_by_columns(t_node *parent);
-void	print_invalids(t_node *head);
-void	print_dir(t_node *parent, unsigned short flags, char *printing_mod);
-
+/*
+** Printing.
+*/
+void							print_one_column(t_node *head,
+									unsigned short flags);
+void							print_by_columns(t_node *parent);
+void							print_invalids(t_node *head);
+void							print_dir(t_node *parent, unsigned short flags,
+									char *printing_mod);
 
 #endif
